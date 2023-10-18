58a868eaf4ca58cbcf819a88c6812ff74e35f7c6
Kacper Piaskowy <Kacper.Piaskowy@zstio.edu.pl>
2023-10-18 23:53:00 +0200

    Database and User model Configuration

---
+++ JobPortal.Api/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using JobPortal.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobPortal.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; } = default!;
+    }
+}
---
+++ JobPortal.Api/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace JobPortal.Api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? ImagePath { get; set; }
+        public string? Location { get; set; }
+        public DateTime? BirthDate { get; set; }
+    }
+}
